ad64dc5a10564fcc6db1425a4f8338334e2b60f1
DANIELLA MARKS <MARKS.DANIELLA@GMAIL.COM>
2021-02-12 11:39:41 -0800

    'refactored code'

--- app.js
+++ app.js
@@ -1,43 +1,76 @@
-// import functions and grab DOM elements
+import { add, subtract, multiply, divide } from '../utilities.js';
 
-// initialize state
 
-// set event listeners on each button, do math, and update state and DOM. serialize. do add completely, do subtract completely, do multiply completely.
+function initOperator(firstInputId, secondInputId, buttonId, resultsId, calculation) {
+    const input1 = document.getElementById(firstInputId);
+    const input2 = document.getElementById(secondInputId);
+    const button = document.getElementById(buttonId);
+    const display = document.getElementById(resultsId);
 
-const add1 = document.getElementById('add1');
-const add2 = document.getElementById('add2');
-const subt1 = document.getElementById('subt1');
+    button.addEventListener('click', () => {
+        const result = calculation(Number(input1.value), Number(input2.value));
+        display.textContent = result;
+    });
+}
+initOperator('add1', 'add2', 'calculate-sum-button', 'add-sum', add);
+initOperator('subt1', 'subt2', 'calculate-diff-button', 'subtractDiff', subtract);
+initOperator('mult1', 'mult2', 'calculate-mult-button', 'multiplyProd', multiply);
+initOperator('div1', 'div2', 'calculate-div-button', 'divQuo', divide);
+    
+
+/*old code: const subt1 = document.getElementById('subt1');
 const subt2 = document.getElementById('subt2');
 const mult1 = document.getElementById('mult1');
 const mult2 = document.getElementById('mult2');
 const div1 = document.getElementById('div1');
 const div2 = document.getElementById('div2');
-const addSum = document.getElementById('add-sum');
 const subtractDiff = document.getElementById('subtract-diff');
 const multiplyProd = document.getElementById('multiply-prod');
 const divQuo = document.getElementById('div-quo');
-const calculateSumButton = document.getElementById('calculate-sum-button');
 const calculateDiffButton = document.getElementById('calculate-diff-button');
 const calculateMultButton = document.getElementById('calculate-mult-button');
 const calculateDivButton = document.getElementById('calculate-div-button');
-
-calculateSumButton.addEventListener('click', () => {
-    const result = Number(add1.value) + Number(add2.value);
-    addSum.textContent = result;
-});
-
 calculateDiffButton.addEventListener('click', () => {
-    const result = Number(subt1.value) - Number(subt2.value);
+    const result = subtract(Number(subt1.value), Number(subt2.value));
     subtractDiff.textContent = result;
 });
-
 calculateMultButton.addEventListener('click', () => {
-    const result = Number(mult1.value) * Number(mult2.value);
+    const result = multiply(Number(mult1.value), Number(mult2.value));
     multiplyProd.textContent = result;
 });
-
 calculateDivButton.addEventListener('click', () => {
-    const result = Number(div1.value) / Number(div2.value);
+    const result = divide(Number(div1.value), Number(div2.value));
     divQuo.textContent = Math.round(result * 100) / 100;
+}); */
+
+const generateRandomButton = document.getElementById('generate-random');
+const randomInt1 = document.getElementById('random-int-1');
+const randomInt2 = document.getElementById('random-int-2');
+
+const randomNumber = Math.floor(Math.random() * 100);
+const randomNumber2 = Math.floor(Math.random() * 100);
+
+generateRandomButton.addEventListener('click', () => {
+    randomInt1.value = randomNumber; 
+    randomInt2.value = randomNumber2;
 });
+
+const userInput = document.getElementById('input-result');
+const checkAnswerButton = document.getElementById('check-answer');
+const showResult = document.getElementById('result');
+
+checkAnswerButton.addEventListener('click', () => {
+    if (Number(userInput.textContent) === randomNumber + randomNumber2) {
+        showResult.textContent = "That's correct!";
+    } else (showResult.textContent = 'Not quite, try again!');
+});
+
+
+
+
+
+
+/*const randomint2 = document.getElementById("random-int-2");
+const inputAnswer = document.getElementById("input-answer");
+*/
 
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,25 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { divide } from '../utilities.js';
+
 const test = QUnit.test;
+
 
 test('time to test a function', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+    // Set up your arguments and expectations. What are the inputs and outputs for the functions you are going to be testing.
+
+    const x = 5;
+    const y = 5;
+    const quotient = 1;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const result = divide(x, y);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    //expect.equal(actual, expected) builtin from the qunit testing library to make an assertion. go test that actual and expected are equal. 
+    expect.equal(result, quotient);
 });
